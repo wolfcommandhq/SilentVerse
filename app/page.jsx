71006c3ade64dbf3bc44f3513972e047be0f7eac
wolfcommandhq <alpha@wolfcommandhq.com>
2025-05-11 23:28:05 -0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white p-8">
+      <h1 className="text-4xl font-bold">SILENTVERSE</h1>
+      <p>Framework detected. Ready for liftoff.</p>
+    </main>
+  )
+}
