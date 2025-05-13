ad10e303a4b5d9d3249b447525fe87b5d96130d0
wolfcommandhq <alpha@wolfcommandhq.com>
2025-05-12 18:15:34 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import Dashboard from '../components/Dashboard'
-import GlobalForum from '../components/GlobalForum'
+import Dashboard from '../../components/Dashboard'
+import GlobalForum from '../../components/GlobalForum'
 
 export default function Home() {
   return (
