d320b312df5e61fb190134686fe7088d38dd4124
wolfcommandhq <alpha@wolfcommandhq.com>
2025-05-12 17:51:30 -0700

    Delete app directory

--- app/layout.jsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="bg-black text-white min-h-screen">
-        {children}
-      </body>
-    </html>
-  )
-}
