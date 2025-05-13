71276929bb1490a242b156442669eb36b5b916dc
wolfcommandhq <alpha@wolfcommandhq.com>
2025-05-12 17:52:52 -0700

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Dashboard from '../components/Dashboard'
+import GlobalForum from '../components/GlobalForum'
+
+export default function Home() {
+  return (
+    <main>
+      <Dashboard />
+      <GlobalForum />
+    </main>
+  )
+}
